1bdd3044fb6cbacac5570db6c55ad22b8798efff
Papryk <patryk040600@gmail.com>
2024-02-26 12:22:44 +0000

    Setup spring boot app

---
+++ src/main/java/com/Inc/Project1/BE/IncProject1BeApplication.java
@@ -0,0 +1,13 @@
+package com.Inc.Project1.BE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IncProject1BeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IncProject1BeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Inc/Project1/BE/IncProject1BeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Inc.Project1.BE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IncProject1BeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
