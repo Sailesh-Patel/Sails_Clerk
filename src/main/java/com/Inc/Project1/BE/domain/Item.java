67b69302bf5ed51d04fefc3005285354135f6ef9
Papryk <patryk040600@gmail.com>
2024-02-27 10:32:25 +0000

    PF: Implemnted domain, repo, controller and services for items and fixed issue with pom.xml

--- src/main/java/com/Inc/Project1/BE/domain/Item.java
+++ src/main/java/com/Inc/Project1/BE/domain/Item.java
@@ -1,5 +1,67 @@
 package com.Inc.Project1.BE.domain;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Item {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	@Column(nullable = false)
+	private String name;
+	@Column(nullable = false)
+	private double quantity;
+	@Column(nullable = false)
+	private String description;
+	@Column(nullable = false)
+	private double price;
+
+	public Item() {
+		super();
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(double quantity) {
+		this.quantity = quantity;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
 
 }
