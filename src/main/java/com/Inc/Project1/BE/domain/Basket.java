7a205264843e49424ca3aac6667b0fb0ed352513
unknown <nath2498@yahoo.co.uk>
2024-02-27 12:14:48 +0000

    Init basket commit. Postman testing stage

--- src/main/java/com/Inc/Project1/BE/domain/Basket.java
+++ src/main/java/com/Inc/Project1/BE/domain/Basket.java
@@ -1,5 +1,58 @@
 package com.Inc.Project1.BE.domain;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Basket {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	@Column(nullable = false)
+	private String name;
+	@Column(nullable = false)
+	private Double price;
+	@Column(nullable = false)
+	private Double quantity;
+
+	public Basket() {
+		super();
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+	public Double getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(Double quantity) {
+		this.quantity = quantity;
+	}
 
 }
