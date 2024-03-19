11c9347ca4a5b964f6a4bf635533223496ded29d
unknown <nath2498@yahoo.co.uk>
2024-03-19 11:05:12 +0000

    Update put to patch for items

--- src/main/java/com/Inc/Project1/BE/rest/ItemController.java
+++ src/main/java/com/Inc/Project1/BE/rest/ItemController.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -48,7 +47,7 @@ public class ItemController {
 	}
 
 	// Update items by ID
-	@PutMapping("/update/{id}")
+	@PatchMapping("/update/{id}")
 	public ResponseEntity<Item> updateItem(@PathVariable int id, @RequestBody Item itemDetails) {
 		return this.service.updateItem(id, itemDetails);
 	}
