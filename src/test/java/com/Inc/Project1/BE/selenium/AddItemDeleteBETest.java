4516f41536c201bd6086fa1fa4dbfc858341e43b
Sailesh-Patel <150167583+Sailesh-Patel@users.noreply.github.com>
2024-04-19 16:01:47 +0100

    Update AddItemDeleteBETest.java

--- src/test/java/com/Inc/Project1/BE/selenium/AddItemDeleteBETest.java
+++ src/test/java/com/Inc/Project1/BE/selenium/AddItemDeleteBETest.java
@@ -19,7 +19,7 @@ import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.jdbc.Sql.ExecutionPhase;
 
-@SpringBootTest(webEnvironment = WebEnvironment.DEFINED_PORT)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @TestMethodOrder(OrderAnnotation.class)
 @Sql(scripts = { "classpath:shop-schema.sql" }, executionPhase = ExecutionPhase.BEFORE_TEST_METHOD)
 
